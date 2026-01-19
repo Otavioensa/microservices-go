103a884f83781d8e92b539ed95dad4aca704f734
Otavio VALENTIM <otavio.valentim@strv.com>
2026-01-19 15:29:58 +0100

    feat: implement graceful shutdown

--- services/api-gateway/main.go
+++ services/api-gateway/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"ride-sharing/shared/env"
 )
@@ -23,7 +28,36 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("Failed to start server: %v", err)
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Println("API Gateway listening on ", httpAddr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutDown := make(chan os.Signal, 1)
+
+	// cmd + c = interrupt
+	// sigterm = signal sent by kubernetes to terminate the app
+	signal.Notify(shutDown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Printf("Error starting server: %v", err)
+	case sig := <-shutDown:
+		log.Printf("Received signal %v, initiating shutdown", sig)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		// starts shutdown in a 10 second window
+		// if it doesn't complete in time, it forces close
+		if err := server.Shutdown(ctx); err != nil {
+			log.Print("Graceful shutdown did not complete in 10s: ", err)
+			server.Close()
+		}
 	}
+
+	// 	log.Printf("Failed to start server: %v", err)
+	// if err := server.ListenAndServe(); err != nil {
+	// }
 }
--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
 	"ride-sharing/shared/env"
+	"syscall"
+	"time"
 )
 
 var (
@@ -29,7 +34,34 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
+	serverErrors := make(chan error, 1)
+
+	go func() {
+		log.Println("Trip service listening on ", httpAddr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	// cmd + c = interrupt
+	// sigterm = signal sent by kubernetes to terminate the app
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
 		log.Printf("Failed to start server: %v", err)
+	case sign := <-shutdown:
+		log.Printf("Received signal %v, initiating shutdown", sign)
+		// timeout of 10 seconds
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Print("Graceful shutdown did not complete in 10s: ", err)
+			server.Close()
+		}
 	}
+
+	// if err := server.ListenAndServe(); err != nil {
+	// 	log.Printf("Failed to start server: %v", err)
+	// }
 }
